51d5b2d371c6bbd812a6dff3bd451fb0d87f4b9e
rajeevkghosh <54052253+rajeevkghosh@users.noreply.github.com>
2021-09-22 12:41:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "google" {
+  access_token = var.access_token
+}
+
+resource "google_compute_instance" "terraform" {
+  project      = "tokyo-nomad-323506"
+  name         = "terraform2"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  labels =  {
+
+  env = "dev"
+  foo = "bar"
+
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
